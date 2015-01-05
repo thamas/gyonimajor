bc246e1e457086fa9dbf3cac569c9ce85becc3f1
Tamás Hajas <hajastamas@gmail.com>
2015-01-05 22:09:06 +0100

    Brace tags build command as Gulp task and and auto build win Gulp watch. Cool! :)

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,22 @@
 var gulp = require('gulp');
 
 // Plugins
+var tags = require('gulp-brace-tags');
 
 // Tasks
+// Brace tags
+gulp.task('tags',function() {
+  return gulp.src("btsrc")
+    .pipe(tags('/usr/local/bin/tags', {
+      out     : '.',
+      debug   : false
+    }))
+});
 
-// Watch
+// Watch files for changes
+gulp.task('watch', function() {
+  gulp.watch('btsrc/**/*.html', ['tags']);
+});
+
+// Default Task
+gulp.task('default', ['tags']);
